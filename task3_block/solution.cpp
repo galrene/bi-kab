488e452f10ea4eadc0b975ce43cb8ed73aef4967
René Gál <galrene@fit.cvut.cz>
2023-04-22 18:32:50 +0200

    prog02: encrypt, decypt, both work but leave some extra bytes

--- task3_block/solution.cpp
+++ task3_block/solution.cpp
@@ -44,35 +44,40 @@ public:
     CCipher ( struct crypto_config & cfg )
     : m_Ctx ( NULL ), m_Cipher ( NULL ), m_Cfg ( cfg ) {}
     ~CCipher () {
-
         EVP_CIPHER_CTX_free(m_Ctx);
     }
     /**
      * Validate supplied config, initialise context and given cipher.
+     * @encrypt true == encrypt mode, false == decrypt mode
      */
     bool init ( bool encrypt );
-    bool encryptFile ( ifstream & ifs, ofstream & ofs );
+    bool updateFile ( ifstream & ifs, ofstream & ofs );
 };
 
 bool CCipher::validateConfig ( bool encrypt ) {
-    int cipherKeyLen = EVP_CIPHER_key_length ( m_Cipher );
-    int cipherIVLen = EVP_CIPHER_iv_length ( m_Cipher );
+    size_t cipherKeyLen = EVP_CIPHER_key_length ( m_Cipher );
+    size_t cipherIVLen = EVP_CIPHER_iv_length ( m_Cipher );
     if ( m_Cfg.m_key == nullptr
         || m_Cfg.m_key_len < cipherKeyLen ) {
-        if ( ! encrypt )
+        if ( ! encrypt ) {
+            cout << "Key error" << endl;
             return false;
+        }
         m_Cfg.m_key = make_unique<uint8_t[]> ( cipherKeyLen );
         m_Cfg.m_key_len = cipherKeyLen;
         RAND_bytes ( m_Cfg.m_key.get(), cipherKeyLen );
     }
-    if ( m_Cfg.m_IV == nullptr || m_Cfg.m_IV_len < cipherIVLen )
-        if ( ! encrypt )
-            return false;
+    if ( m_Cfg.m_IV == nullptr || m_Cfg.m_IV_len < cipherIVLen ) {
         if ( cipherIVLen ) {
+            if ( ! encrypt ) {
+                cout << "IV error" << endl;
+                return false;
+            }
             m_Cfg.m_IV = make_unique<uint8_t[]> ( cipherIVLen );
             m_Cfg.m_IV_len = cipherIVLen;
             RAND_bytes ( m_Cfg.m_IV.get(), cipherIVLen );
         }
+    }
     return true;
 }
 
@@ -84,7 +89,7 @@ bool CCipher::validateConfig ( bool encrypt ) {
  * without allocating and freeing it up on each call.
  */
 
-bool CCipher::encryptFile ( ifstream & ifs, ofstream & ofs ) {
+bool CCipher::updateFile ( ifstream & ifs, ofstream & ofs ) {
     char inBuff[INBUFF_CAP] = {};
     char outBuff[OUTBUFF_CAP] = {};
     int outSize = 0;
@@ -92,73 +97,110 @@ bool CCipher::encryptFile ( ifstream & ifs, ofstream & ofs ) {
         ifs.read ( inBuff, INBUFF_CAP );
         if ( ! EVP_CipherUpdate (m_Ctx,
         reinterpret_cast<unsigned char *>(outBuff), &outSize,
-        reinterpret_cast<const unsigned char *>(inBuff), INBUFF_CAP ) )
+        reinterpret_cast<const unsigned char *>(inBuff), INBUFF_CAP ) ) {
+            cout << "Update failed" << endl;
             return false;
+        }
         ofs.write ( outBuff, outSize );
     }
     // finished reading infile
     if ( ifs.eof() ) {
-        if ( ! EVP_CipherFinal_ex ( m_Ctx, reinterpret_cast<unsigned char *>(outBuff), &outSize ) )
+        if ( ! EVP_CipherFinal_ex ( m_Ctx, reinterpret_cast<unsigned char *>(outBuff), &outSize ) ) {
+            cout << "Final failed" << endl;
             return false;
+        }
         ofs.write ( outBuff, outSize );
-        if ( ! ofs.good() ) return false;
+        if ( ! ofs.good() ) {
+            cout << "Final write failed" << endl;
+            return false;
+        }
         return true;
     }
     return false;
 }
 
 bool CCipher::init ( bool encrypt ) {
-    if ( m_Ctx = EVP_CIPHER_CTX_new(); m_Ctx == NULL )
+    if ( m_Ctx = EVP_CIPHER_CTX_new(); ! m_Ctx ) {
+        cout << "Context creation" << endl;
         return false;
+    }
     OpenSSL_add_all_ciphers();
-    if ( m_Cipher = EVP_get_cipherbyname (m_Cfg.m_crypto_function ); ! m_Cipher )
+    if ( m_Cipher = EVP_get_cipherbyname (m_Cfg.m_crypto_function ); ! m_Cipher ) {
+        cout << "Cipher name not found" << endl;
         return false;
-    if ( ! validateConfig ( encrypt ) )
+    }
+    if ( ! validateConfig ( encrypt ) ) {
+        cout << "Cfg validation failed" << endl;
         return false;
-    if ( ! EVP_CipherInit_ex2( m_Ctx, m_Cipher, m_Cfg.m_key.get(), m_Cfg.m_IV.get(), static_cast<int> ( encrypt ), NULL ) )
+    }
+    if ( ! EVP_CipherInit_ex2( m_Ctx, m_Cipher, m_Cfg.m_key.get(), m_Cfg.m_IV.get(), static_cast<int> ( encrypt ), NULL ) ) {
+        cout << "Init failed" << endl;
         return false;
+    }
     return true;
 }
 
-
-bool encrypt_data ( const std::string & in_filename, const std::string & out_filename, crypto_config & config ) {
-    CCipher c ( config );
-    ifstream ifs ( in_filename );
-    ofstream ofs ( out_filename );
-    if ( ! ifs.good() || ! ofs.good() ) {
-        cout << "Unable to open file" << endl;
-        return false;
-    }
+bool copyHeader ( ifstream & ifs, ofstream & ofs ) {
     char header[18] = {0};
     ifs.read ( header, 18 );
     if ( ifs.gcount() != 18 ) {
         cout << "Unable to read header" << endl;
         return false;
     }
-    if ( ! c.init ( true ) || ! c.encryptFile ( ifs, ofs ) )
+    ofs.write ( header, 18 );
+    if ( ! ofs.good() )
         return false;
-    // close ifs, ofs
+    return true;
+}
+
+bool crypt_ex ( const std::string & in_filename, const std::string & out_filename, crypto_config & config, bool encrpyt ) {
+    CCipher c ( config );
+    ifstream ifs ( in_filename );
+    ofstream ofs; ofs.open ( out_filename );
+    if ( ! ifs.good() || ! ofs.good() ) {
+        cout << "Unable to open file" << endl;
+        return false;
+    }
+    if ( ! copyHeader ( ifs, ofs ) ) {
+        cout << "Unable to copy header" << endl;
+        return false;
+    }
+    if ( ! c.init ( encrpyt ) || ! c.updateFile ( ifs, ofs ) ) {
+        cout << "Cipher failed" << endl;
+        return false;
+    }
+    // close ifs, ofs?
+    return true;
+}
+
+bool encrypt_data ( const std::string & in_filename, const std::string & out_filename, crypto_config & config ) {
+    return crypt_ex(in_filename, out_filename, config, true );
 }
 
 bool decrypt_data ( const std::string & in_filename, const std::string & out_filename, crypto_config & config ) {
-
+    return crypt_ex(in_filename, out_filename, config, false );
 }
 
 
 #ifndef __PROGTEST__
+#include <filesystem>
 // TODO: test for files with different lengths!
-bool compare_files ( const char * name1, const char * name2 )
-{
+bool compare_files ( const char * name1, const char * name2 ) {
+    namespace fs = std::filesystem;
+    if ( fs::file_size(name1) != fs::file_size(name2) ) {
+        cout << "File size mismatch" << endl;
+        return false;
+    }
     ifstream ifs1 (name1);
     ifstream ifs2 (name2);
     string word;
     string word2;
     while ( ifs1 >> word && ifs2 >> word2 ) {
-        if ( word != word2)
+        if ( word != word2 ) {
+            cout << "Files not equal" << endl;
             return false;
+        }
     }
-    if ( word != word2)
-        return false;
     return true;
 }
 
