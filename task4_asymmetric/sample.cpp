32940e2ecf2219a24b0d1846c3893bad591fc5ec
René Gál <galrene@fit.cvut.cz>
2023-05-01 13:06:05 +0200

    prog03: func for writing header of outfile

--- task4_asymmetric/sample.cpp
+++ task4_asymmetric/sample.cpp
@@ -42,7 +42,7 @@ private:
     TCryptoConfig m_Cfg;
     unsigned char m_IV[EVP_MAX_IV_LENGTH] = {};
     unsigned char * m_EncKey;
-    int m_EncKeyCnt;
+    int m_EncKeyLen;
 
     EVP_PKEY * m_PKey;
     ifstream m_Infile;
@@ -50,19 +50,18 @@ private:
 public:
     explicit CHybridCipher ( const TCryptoConfig & cfg )
     : m_Ctx ( NULL ), m_Cipher ( NULL ), m_Cfg ( cfg ),
-      m_EncKey ( NULL ), m_EncKeyCnt ( 0 ), m_PKey ( NULL ) {}
+      m_EncKey ( NULL ), m_EncKeyLen ( 0 ), m_PKey ( NULL ) {}
 
     ~CHybridCipher();
 
     bool updateFile ();
 
-    bool init ( bool encrypt );
+    bool init ();
+
+    bool writeHeader();
 };
 
 CHybridCipher::~CHybridCipher() {
-    // TODO: you want to del only on failure
-//    if ( m_Outfile.is_open() )
-//        std::remove ( m_Cfg.m_Outfile );
     if ( m_PKey )
         EVP_PKEY_free ( m_PKey );
 }
@@ -91,8 +90,7 @@ bool CHybridCipher::updateFile () {
     return false;
 }
 
-
-bool CHybridCipher::init ( bool encrypt ) {
+bool CHybridCipher::init () {
     OpenSSL_add_all_ciphers();
     if ( ! m_Cfg.m_InFile || ! m_Cfg.m_Outfile || ! m_Cfg.m_PublicKeyFile || ! m_Cfg.m_Cipher )
         return false;
@@ -112,21 +110,55 @@ bool CHybridCipher::init ( bool encrypt ) {
     fclose ( pkFile );
     if ( ! m_PKey )
         return false;
-
+    // TODO: m_EncKey probably wrong
     if ( ! EVP_SealInit ( m_Ctx, m_Cipher,
-                          &m_EncKey, &m_EncKeyCnt , m_IV,
+                          &m_EncKey, &m_EncKeyLen , m_IV,
                           &m_PKey, EVP_PKEY_size ( m_PKey ) ) )
+        return false;
+    return true;
+}
+/**
+    Pozice v souboru 	Délka 	    Struktura 	        Popis
+    0 	                4 B 	    int 	            NID - numerical identifier for an OpenSSL cipher. (Použitá symetrická šifra)
+    4 	                4 B 	    int 	            EKlen - délka zašifrovaného klíče
+    8 	                EKlen B 	pole unsigned char 	Zašifrovaný klíč pomocí RSA
+    8 + EKlen 	        IVlen B 	pole unsigned char 	Inicializační vektor (pokud je potřeba)
+    8 + EKlen + IVlen 	  —    	    pole unsigned char 	Zašifrovaná data
+ */
+bool CHybridCipher::writeHeader() {
+    if ( ! m_Outfile.good() )
+        return false;
+    stringstream ss;
+
+    ss << EVP_CIPHER_nid ( m_Cipher );
+    m_Outfile.write ( ss.str().c_str(), ss.str().size() ); // write NID
+    ss.clear();
+
+    ss << m_EncKeyLen;
+    m_Outfile.write ( ss.str().c_str(), ss.str().size() ); // write EKlen
+    ss.clear();
+
+    ss << m_EncKey;
+    m_Outfile.write ( ss.str().c_str(), ss.str().size() ); // write EK
+    ss.clear();
+
+    ss << m_IV;
+    m_Outfile.write ( ss.str().c_str(), ss.str().size() ); // write IV
+    ss.clear();
+
+    if ( ! m_Outfile.good() )
         return false;
     return true;
 }
 
 bool seal ( const char * inFile, const char * outFile, const char * publicKeyFile, const char * symmetricCipher ) {
-    CHybridCipher cipher ( { inFile, outFile, publicKeyFile, symmetricCipher } );
-    if ( ! cipher.init ( true ) )
+    CHybridCipher c ( { inFile, outFile, publicKeyFile, symmetricCipher } );
+    if ( ! c.init () || ! c.writeHeader() || ! c.updateFile() ) {
+        std::remove ( outFile ); // TODO: won't break things in destructors?
         return false;
+    }
     return true;
 }
-
 
 bool open ( const char * inFile, const char * outFile, const char * privateKeyFile ) {
     //waiting for code...
